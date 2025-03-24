274153a739e35b1c2b117eb07325ffda0b3623b4
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2025-03-24 08:29:49 +0100

    New

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import Email from "./components/email";
-import Tel from "./components/tel";
-import Url from "./components/url";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Principale from './pages/Principale.jsx';
+import NavBar from './composants/NavBar.jsx';
+import Connexion from './pages/Connexion.jsx';
+import Inscription from './pages/Inscription.jsx';
 
 function App() {
 
   return (
-    <Email />
+
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Principale />} />
+        <Route path="/Inscription" element={<Inscription />} />
+        <Route path="/Connexion" element={<Connexion />} />
+      </Routes>
+    </Router>
+
   );
 
 }
