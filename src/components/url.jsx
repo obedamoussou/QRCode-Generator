b39356567d14acd1a27771be482b7a79d2eb636b
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2025-03-25 08:54:58 +0100

    pushing branch Rosme

--- src/components/url.jsx
+++ src/components/url.jsx
@@ -55,31 +55,38 @@ function Url () {
         setLogoWidth(tempLogoWidth);
       };
       
+      const options = [
+        { value: 'option1', label: <input type="file" accept="image/*" onChange={handleImageUpload} className="mb-4" /> },
+        { value: 'option2', label: <label htmlFor="" className="start text-lg font-medium">Hauteur <input type="number" className="border p-2 rounded-md w-72 mb-4" onChange={(e) => setTempLogoHeight(e.target.value)} /></label> },
+        { value: 'option3', label: <label htmlFor="" className="text-lg font-medium">Largeur <input type="number" id="" className="border p-2 rounded-md w-72 mb-4" onChange={(e) => setTempLogoWidth(e.target.value)} /> </label> },
+        
+      ];
     return(
       <section> 
          
         <div className="flex flex-wrap gap-y-5 gap-x-10">
           
-            <form className="items-start" action="">
-              
+            <form className="flex flex-col items-start gap-y-5" action="">
+              <h1 className="text-2xl font-bold text-[#0000FF]">Lien/URL</h1>
               <input 
                 type="url"
                 value={ url}
-                className={`border p-2 rounded-md w-80 mb-4 ${error && "border-red-500"}`}
+                className={`text-[#0000FF] border-2 border-[#0000FF] p-2 rounded-md w-80 mb-4 ${error && "border-red-500"}`}
                 onChange={(e) => setUrl(e.target.value)}
+                placeholder="Entrez une URL"
               />
                 
                 {error && <p className="text-red-500">{error}</p>}
 
                 <button
                     onClick={handleClick}
-                    className="bg-blue-500 text-white px-4 py-2 rounded-md">
+                    className="bg-[#0000FF] text-white px-4 py-2 rounded-2xl">
                     Générer QR Code
                 </button>
             </form>
 
-            <div className="bg-blue-50 rounded-xl p-8 items-center">
-                    
+            <div className="bg-blue-50 rounded-xl  justify-center">
+                  
                 {qrValue && <QRCodeSVG value={qrValue} fgColor={color} bgColor={bgColor} size={170} imageSettings={
                     imageInt
                     ? {
@@ -90,9 +97,10 @@ function Url () {
                       }
                     : undefined
                      } />}
+                    
 
                 <div className=" items-center gap-4 mb-4">
-                    <label className="text-lg font-medium">Couleur :
+                    <label className="text-lg font-medium">Couleur 
                     <input
                     type="color"
                     value={color}
@@ -107,22 +115,21 @@ function Url () {
                     className="w-10 h-10 p-1 border rounded-md"
                     />
                 </div>
-                <div className="flex flex-col">
-                    
-
-                    <label htmlFor="" className="text-lg font-medium">Image Width :
-                    <input type="file" accept="image/*" onChange={handleImageUpload} className="mb-4" />
-                    </label>
-
-                    <label htmlFor="" className="start text-lg font-medium">Image Heigth :  
-                    <input type="number" className="border p-2 rounded-md w-80 mb-4" onChange={(e) => setTempLogoHeight(e.target.value)} />
-                    </label>
-
-                    <label htmlFor="" className="text-lg font-medium">Image Width :  
-                    <input type="number" id="" className="border p-2 rounded-md w-80 mb-4" onChange={(e) => setTempLogoWidth(e.target.value)} />
-                    </label>
-              
-                    
+                <div className="mb-4">
+                  <label htmlFor="select" className="block text-sm font-medium text-gray-700">
+                    Sélectionnez une option
+                  </label>
+                  <select
+                    id="select"
+                    className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+                  >
+                    <option value="" disabled>Sélectionnez une option</option>
+                    {options.map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                    ))}
+                  </select>
                 </div>     
             </div>
         </div>
