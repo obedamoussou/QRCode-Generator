9f3af2d95a285a4376dc6ec5439603e2333e448b
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2025-03-25 17:23:58 +0100

    Animation

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,14 +1,22 @@
 
 import React from 'react';
+import AnimationIcones from '../composants/AnimationIcones';
 
 
 const Accueil = () => {
     return (
-        <div className="accueil">
-            <h1>Bienvenue sur la page d'accueil !</h1>
-            <p>Ceci est un exemple de composant Accueil.</p>
-            <p className="doto-font">Ce texte utilise la police Doto.</p>
-        </div>
+        <section>
+            <div className='flex flex-wrap justify-center gap-y-4 gap-x-2 md:gap-6 xl:gap-x-32 py-8 lg:py-10'>
+                <div className='pt-0 lg:pt-4'>
+                    <h1 className='doto text-[#0000FF]  text-2xl xl:text-4xl w-64'>Générez facilement vos codes QR avec QREasy</h1>
+                    <p className='text-md lg:text-lg w-72 py-5'>Créez, customisez et téléchargez au même endroit vos codes QR en quelques clics.</p>
+                    <button className="bg-[#0000FF] text-white font-bold px-4 py-2 rounded-lg doto text-lg"><a href="/src/pages/CodeQR.jsx">Commencer</a></button>
+                </div>
+                <div className='pt-0 lg:pt-8'>
+                    <AnimationIcones />
+                </div>
+            </div>
+        </section>
     );
 };
 
